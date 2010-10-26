0be634b98648f8d9ee1827f749cad277abbfb486
Martin Tithonium <martian@midgard.org>
2010-10-26 15:54:55 -0700

    Fixing bar graphs with ignore_set_spacing

--- lib/prawn/graph/bar.rb
+++ lib/prawn/graph/bar.rb
@@ -68,7 +68,7 @@ module Prawn
             if value
               @document.stroke_color @theme.next_colour
               if @direction == :horizontal
-                y_position = calculate_y_offset(heading, idx) + (barYOffset * setidx)
+                y_position = calculate_y_offset(heading, idx) + (barYOffset * (setidx + 1))
                 bar_length = calculate_point_width_from value
                 @document.move_to [@inverted ? far_x : base_x, y_position]
                 @document.stroke_line_to [base_x + bar_length, y_position]
